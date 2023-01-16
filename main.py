61597ed8f5cf06ccf1a34c0d05cb0724c1176b7b
daniel <daniel.montoyav@gmail.com>
2023-01-16 23:03:57 +0100

    Second lesson

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from kivy.app import App
+from kivy.metrics import dp
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(10):
+            b = Button(text=str(i+1), size_hint=(None, None), size=(dp(100), dp(100)))
+            self.add_widget(b)
+# class GridLayoutExample(GridLayout):
+#    pass
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
 
 class BoxLayoutExample(BoxLayout):
     pass
+
+
 """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -16,10 +34,14 @@ class BoxLayoutExample(BoxLayout):
         self.add_widget(b2)
         self.add_widget(b3)
 """
+
+
 class MainWidget(Widget):
     pass
 
+
 class TheLabApp(App):
     pass
+
 
 TheLabApp().run()
