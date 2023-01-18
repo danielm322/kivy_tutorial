98c99c2291fed888c9cfdfe8c08b11e8dc4c973c
daniel <daniel.montoyav@gmail.com>
2023-01-18 22:18:36 +0100

    Third lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -7,10 +8,35 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
+
+class WidgetsExample(GridLayout):
+    counter = 0
+    count_enabled = BooleanProperty(False)
+    my_text = StringProperty("Hello!")
+    def on_button_click(self):
+        print("Button clicked")
+        if self.count_enabled:
+            self.counter += 1
+            self.my_text = f"You clicked {self.counter} times"
+
+    def on_toggle_button_state(self, widget):
+        print("Toggle state:" + widget.state)
+        if widget.state == "normal":
+            widget.text = "OFF"
+            self.count_enabled = False
+        else:
+            widget.text = "ON"
+            self.count_enabled = True
+
+    def on_switch_active(self, widget):
+        print("Switch: " + str(widget.active))
+
+    def on_slider_value(self, widget):
+        print("Slider: " + str(widget.value))
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        for i in range(10):
+        for i in range(100):
             b = Button(text=str(i+1), size_hint=(None, None), size=(dp(100), dp(100)))
             self.add_widget(b)
 # class GridLayoutExample(GridLayout):
